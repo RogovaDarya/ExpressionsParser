101144661fce51165fc336e0fb4bf2f269a385c5
RogovaDarya <dasharogova04@gmail.com>
2024-10-15 11:18:07 +0300

    Add project files.

---
+++ Expression/Expr.cs
@@ -0,0 +1,80 @@
+﻿namespace Expressions;
+
+public static class Expr
+{
+    private static string _expression = "";
+    private static int _index = 0;
+
+    public static bool ParseExpr(string expression)
+    {
+        _index = 0;
+        _expression = expression;
+        bool valid = Parse_expression();
+        return valid && _index == _expression.Length;
+    }
+
+    private static bool Parse_expression()
+    {
+        if (_index >= _expression.Length) return false;
+
+        SkipWhitespace();
+
+        // Обработка унарного минуса
+        if (_expression[_index] == '-')
+        {
+            _index++;
+            if (!Parse_expression()) return false;
+            return true;
+        }
+
+        SkipWhitespace();
+
+        // Обработка чисел и скобок
+        if (Char.IsDigit(_expression[_index]))
+        {
+            ParseNumber();
+        }
+        else if (_expression[_index] == '(')
+        {
+            _index++;
+            if (!Parse_expression()) return false;
+            if (_index >= _expression.Length || _expression[_index] != ')') return false;
+            _index++;
+        }
+        else
+        {
+            return false;
+        }
+
+        // Пропуск пробелов
+        SkipWhitespace();
+
+        // Обработка операторов
+        if (_index < _expression.Length && IsOperator(_expression[_index]))
+        {
+            _index++;
+            return Parse_expression();
+        }
+
+        return true;
+    }
+
+    private static bool IsOperator(char v) 
+        => v is '+' or '-' or '*' or '/';
+
+    private static void ParseNumber()
+    {
+        while (_index < _expression.Length && Char.IsDigit(_expression[_index]))
+        {
+            ++_index;
+        }
+    }
+
+    private static void SkipWhitespace()
+    {
+        while (_index < _expression.Length && Char.IsWhiteSpace(_expression[_index]))
+        {
+            ++_index;
+        }
+    }
+}
---
+++ Expressions.Test/ExpressionTest.cs
@@ -0,0 +1,67 @@
+﻿namespace Expressions.Test;
+
+[TestFixture]
+public class ExpressionTest
+{
+    [TestCase("(1 + 1)", ExpectedResult = true)]
+    [TestCase("(2+1)*2", ExpectedResult = true)]
+    [TestCase("(2)", ExpectedResult = true)]
+    [TestCase("3+(4*5)-2", ExpectedResult = true)]
+    [TestCase("((10/(5+5))*(6-4))", ExpectedResult = true)]
+    [TestCase("(7+8*(9-3))/(2+1)", ExpectedResult = true)]
+    [TestCase("(2+(3*(4-2)))", ExpectedResult = true)]
+    [TestCase("(10/(5+(1*2)))", ExpectedResult = true)]
+    [TestCase("((2+3)*(4/2))", ExpectedResult = true)]
+    [TestCase("((6-2)*(8+3))/(4+1)", ExpectedResult = true)]
+    [TestCase("(10*(2+3))/5", ExpectedResult = true)]
+    [TestCase("(15/(3+2))*(4-2)", ExpectedResult = true)]
+    [TestCase("(9-(4+5))/2", ExpectedResult = true)]
+    [TestCase("(7*(8-3))+4", ExpectedResult = true)]
+    [TestCase("(6/(2+1))*(9-4)", ExpectedResult = true)]
+    [TestCase("((2*3)+(4-2))", ExpectedResult = true)]
+    [TestCase("(12+(5*2))/7", ExpectedResult = true)]
+    [TestCase("(10-(3+2))*(4+1)", ExpectedResult = true)]
+    [TestCase("((4+6)*(8/2))/(5-1)", ExpectedResult = true)]
+    [TestCase("(7-(2+3))*(9+1)", ExpectedResult = true)]
+    [TestCase("-2", ExpectedResult = true)]
+    [TestCase("-(2)", ExpectedResult = true)]
+    [TestCase("-(1+2)", ExpectedResult = true)]
+    [TestCase("-2+1", ExpectedResult = true)]
+    [TestCase("-2-(2)", ExpectedResult = true)]
+    [TestCase("122 + 1", ExpectedResult = true)]
+    [TestCase("122 + 1 ", ExpectedResult = true)]
+    [TestCase(" 122 + 1", ExpectedResult = true)]
+    [TestCase("122 + (1 ) ", ExpectedResult = true)]
+    [TestCase("122 + ( 1)", ExpectedResult = true)]
+    [TestCase("122 + ( 1 )", ExpectedResult = true)]
+    [TestCase("122 + (   1  )", ExpectedResult = true)]
+    [TestCase("122 + (   1)", ExpectedResult = true)]
+    [TestCase("   122 + ( 1)", ExpectedResult = true)]
+    public bool ExpressionTestTrue (string expression)
+    {
+        return Expr.ParseExpr(expression);
+    }
+
+    [TestCase("((", ExpectedResult = false)]
+    [TestCase("(2)+", ExpectedResult = false)]
+    [TestCase("2+)+3", ExpectedResult = false)]
+    [TestCase("(5*(6-3)", ExpectedResult = false)]
+    [TestCase("((4/2))*(3+)", ExpectedResult = false)]
+    [TestCase("(3+4)*5)-2", ExpectedResult = false)]
+    [TestCase("(2*(3+(4-2))", ExpectedResult = false)]
+    [TestCase("(10/(5+2)*(6-4", ExpectedResult = false)]
+    [TestCase("(3/0))", ExpectedResult = false)]
+    [TestCase("((6-2)*(8+))/5", ExpectedResult = false)]
+    [TestCase("((10*(2+3))/5", ExpectedResult = false)]
+    [TestCase("((15/(3+2)*(4-2)", ExpectedResult = false)]
+    [TestCase("(9-(4+5)/2", ExpectedResult = false)]
+    [TestCase("7*(8-3))+", ExpectedResult = false)]
+    [TestCase("(6/(2+1)*(9-", ExpectedResult = false)]
+    [TestCase("(2*3)+(4-)", ExpectedResult = false)]
+    [TestCase("10-(3+2)*(4+", ExpectedResult = false)]
+    [TestCase("(7-(2+3)*(9+", ExpectedResult = false)]
+    public bool ExpressionTestFalse(string expression)
+    {
+        return Expr.ParseExpr(expression);
+    }
+}
